da965fc55d023885874ee3fcbac703d261bea6c5
ahntaewoong <lgtwins.ahn@gmail.com>
2020-05-14 16:08:21 +0900

    ahntaewoong

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,8 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 def home(request):
-    return HttpResponse("Hello, Django!")
+    name01 = request.GET['name']
+    age02 = request.GET['age']
+    requestDict = request.GET
+    result = int(age02) + 5
+    requestDict = {'result_response':result}
+    return JsonResponse(requestDict)
